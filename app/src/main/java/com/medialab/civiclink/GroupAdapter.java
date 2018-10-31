e3ade21fca51ebed6c0d4dba96ad6a1f9e2d193e
Jenny <jz.skittles@gmail.com>
2018-10-31 18:34:41 -0400

    add search bar, custom filtering based on text, separate groups you're in from groups you're not in, new php added, troubleshooting

--- app/src/main/java/com/medialab/civiclink/GroupAdapter.java
+++ app/src/main/java/com/medialab/civiclink/GroupAdapter.java
@@ -1,18 +1,27 @@
 package com.medialab.civiclink;
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-public class GroupAdapter extends BaseAdapter{
-    private Context mContext;
-    private final String[] web;
-    private final int[] Imageid;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 
-    public GroupAdapter(Context c,String[] web,int[] Imageid ) {
+public class GroupAdapter extends BaseAdapter implements Filterable {
+    private Context mContext;
+    private ArrayList<String> web;
+    private int[] Imageid;
+
+    ArrayList<String> modelValues = new ArrayList<>();
+
+    public GroupAdapter(Context c, ArrayList<String> web, int[] Imageid ) {
         mContext = c;
         this.Imageid = Imageid;
         this.web = web;
@@ -21,7 +30,16 @@ public class GroupAdapter extends BaseAdapter{
     @Override
     public int getCount() {
         // TODO Auto-generated method stub
-        return web.length;
+        return web.size();
+    }
+
+    public void setWeb(ArrayList<String> web){
+        this.web = web;
+        int[] imageId2 = new int[web.size()];
+        for(int j=0;j<imageId2.length;j++) {
+            imageId2[j] = R.drawable.groups;
+        }
+        this.Imageid = imageId2;
     }
 
     @Override
@@ -51,10 +69,73 @@ public class GroupAdapter extends BaseAdapter{
         }
 
         TextView textView = (TextView) grid.findViewById(R.id.grouptext);
-        ImageView imageView = (ImageView)grid.findViewById(R.id.groupimg);
-        textView.setText(web[position]);
+        ImageView imageView = (ImageView) grid.findViewById(R.id.groupimg);
+        textView.setText(web.get(position));
         imageView.setImageResource(Imageid[position]);
 
         return grid;
+    }
+
+    @Override
+    public Filter getFilter() {
+
+        Filter filter = new Filter() {
+
+            @SuppressWarnings("unchecked")
+            @Override
+            protected void publishResults(CharSequence constraint, FilterResults results) {
+                web = (ArrayList<String>) results.values; // has
+
+                notifyDataSetChanged();
+            }
+
+            @Override
+            protected FilterResults performFiltering(CharSequence constraint) {
+                FilterResults results = new FilterResults(); // Holds the
+                // results of a
+                // filtering
+                // operation in
+                // values
+                // List<String> FilteredArrList = new ArrayList<String>();
+                List<String> FilteredArrList = new ArrayList<>();
+
+                /*if (web == null) {
+                    web = new ArrayList<>(modelValues); // saves
+
+                }*/
+
+                /********
+                 *
+                 * If constraint(CharSequence that is received) is null returns
+                 * the mOriginalValues(Original) values else does the Filtering
+                 * and returns FilteredArrList(Filtered)
+                 *
+                 ********/
+                if (constraint == null || constraint.length() == 0) {
+
+                    // set the Original result to return
+                    results.count = web.size();
+                    results.values = web;
+                } else {
+                    Locale locale = Locale.getDefault();
+                    constraint = constraint.toString().toLowerCase(locale);
+                    for (int i = 0; i < web.size(); i++) {
+                        String model = web.get(i);
+
+                        //String data = model.get_NAME();
+                        if (model.toLowerCase(locale).contains(constraint.toString())) {
+
+                            FilteredArrList.add(model);
+                        }
+                    }
+                    // set the Filtered result to return
+                    results.count = FilteredArrList.size();
+                    results.values = FilteredArrList;
+
+                }
+                return results;
+            }
+        };
+        return filter;
     }
 }
--- app/src/main/java/com/medialab/civiclink/Groups.java
+++ app/src/main/java/com/medialab/civiclink/Groups.java
@@ -1,15 +1,28 @@
 package com.medialab.civiclink;
 
+import android.app.SearchManager;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.GridLayout;
 import android.support.v7.widget.CardView;
 import android.widget.GridView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.volley.Request;
@@ -18,34 +31,71 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.mancj.materialsearchbar.MaterialSearchBar;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
-public class Groups extends AppCompatActivity {
-    GridView mainGrid;
-    GroupAdapter adapter;
+public class Groups extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+    GridView mainGrid, yourGrid, totalGrid;
+    GroupAdapter adapter, adapter2, adaptertotal;
 
     private RequestQueue requestQueue;
 
-    private StringRequest request;
+    private StringRequest request, request2;
 
     SessionManagement session;
 
     String url = "http://civiclink.000webhostapp.com/groups.php?user=";
+    String url2 = "http://civiclink.000webhostapp.com/othergroups.php?user=";
     String email="";
 
-    String[] web= new String[0];
+    ArrayList<String> web = new ArrayList<>();
+    ArrayList<String> web2 = new ArrayList<>();
+    ArrayList<String> webtotal = new ArrayList<>();
+
+
+    Button new_group, join_group;
+
+    int previousLength;
+    boolean backSpace;
+
+    TextView yourgroup, allgroup, totalgroup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_groups);
 
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+        navigationView.setItemIconTintList(null);
+
         mainGrid = (GridView) findViewById(R.id.mainGrid);
+        yourGrid = (GridView) findViewById(R.id.yourGrid);
+        totalGrid = (GridView) findViewById(R.id.totalGrid);
+
+        yourgroup = (TextView)findViewById(R.id.yourGroups);
+        allgroup = (TextView)findViewById(R.id.allGroups);
+        totalgroup = (TextView)findViewById(R.id.totalGroups);
+
+        //INITIALIZE GRIDVIEW AND MATERIAL SEARCHBAR
+        MaterialSearchBar searchBar = (MaterialSearchBar) findViewById(R.id.searchBar);
+        searchBar.setHint("Search..");
+        searchBar.setSpeechMode(true);
 
         requestQueue = Volley.newRequestQueue(this);
 
@@ -54,7 +104,16 @@ public class Groups extends AppCompatActivity {
         HashMap<String, String> user = session.getUserDetails();
         email = user.get(SessionManagement.KEY_EMAIL);
 
+        new_group = (Button)findViewById(R.id.new_group);
+        new_group.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), CreateGroup.class));
+            }
+        });
+
         url = url+email;
+        url2 = url2+email;
 
         request = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
             @Override
@@ -67,25 +126,29 @@ public class Groups extends AppCompatActivity {
                             JSONObject row = jsonObject.getJSONObject(i+"");
 
                             String ingroups = row.getString("groups");
+                            String [] temp;
                             if(ingroups.contains(",")){
-                                web = ingroups.split(",");
+                                temp = ingroups.split(",");
                             }else{
-                                web = new String[1];
-                                web[0] = ingroups;
+                                temp = new String[1];
+                                temp[0] = ingroups;
                             }
+                            for(int j=0;j<temp.length;j++){
+                                web.add(temp[j]);
+                                webtotal.add(temp[j]);
+                            }
+
                             Log.e("GROUPS", ingroups);
-                            Log.e("GROUPS", web[0]);
+                            Log.e("GROUPS", web.get(0));
 
-                            int[] imageId = {
-                                    R.drawable.groups,
-                                    R.drawable.groups,
-                                    R.drawable.groups,
-                                    R.drawable.groups
-                            };
+                            int[] imageId = new int[web.size()];
+                            for(int j=0;j<imageId.length;j++) {
+                                imageId[j] = R.drawable.groups;
+                            }
 
-                            GroupAdapter adapter = new GroupAdapter(getApplicationContext(), web, imageId);
-                            mainGrid.setAdapter(adapter);
-                            mainGrid.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                            adapter = new GroupAdapter(getApplicationContext(), web, imageId);
+                            yourGrid.setAdapter(adapter);
+                            yourGrid.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
                                 @Override
                                 public void onItemClick(AdapterView<?> parent, View view,
@@ -119,5 +182,236 @@ public class Groups extends AppCompatActivity {
         requestQueue.add(request);
 
 
+
+        request2 = new StringRequest(Request.Method.GET, url2, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try{
+                    JSONObject jsonObject = new JSONObject(response);
+                    if(jsonObject.has("success")){
+                        int length = jsonObject.getInt("length");
+                        for(int i = 0; i < length-1; i++) {
+                            JSONObject row = jsonObject.getJSONObject(i+"");
+
+                            String groupname = row.getString("groupname");
+                            String groupdetails = row.getString("groupdetails");
+                            String address = row.getString("groupaddress");
+                            String grouptype = row.getString("grouptype");
+                            String groupmembers = row.getString("groupmembers");
+                            String code = row.getString("code");
+                            String admin = row.getString("admin");
+                            web2.add(groupname);
+                            webtotal.add(groupname);
+
+                            Log.e("GROUPS", web2.get(0));
+
+                            int[] imageId = new int[web2.size()];
+                            for(int j=0;j<imageId.length;j++) {
+                                imageId[j] = R.drawable.groups;
+                            }
+
+                            adapter2 = new GroupAdapter(getApplicationContext(), web2, imageId);
+                            mainGrid.setAdapter(adapter2);
+                            mainGrid.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+                                @Override
+                                public void onItemClick(AdapterView<?> parent, View view,
+                                                        int position, long id) {
+                                    Toast.makeText(getApplicationContext(), "You Clicked at " +position, Toast.LENGTH_SHORT).show();
+
+                                }
+                            });
+
+                        }
+                        //Toast.makeText(getApplicationContext(), "SUCCESS: " + jsonObject.getString("success"), Toast.LENGTH_SHORT).show();
+                    }else{
+                        if(jsonObject.has("empty")) {
+                            Toast.makeText(getApplicationContext(),"EMPTY: "+jsonObject.getString("empty"),Toast.LENGTH_SHORT).show();
+                        }else {
+                            Toast.makeText(getApplicationContext(), "ERROR: " + jsonObject.getString("error"), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }catch(JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener(){
+            @Override
+            public void onErrorResponse(VolleyError error){
+
+            }
+
+        });
+
+        requestQueue.add(request2);
+
+        //final ArrayList<String> webtotal = new ArrayList<>();
+
+        final List<String> grouplist = new ArrayList<>();
+
+        for(int x=0;x<web.size();x++){
+            grouplist.add(web.get(x));
+            //webtotal.add(web.get(x));
+        }
+        for(int x=0;x<web2.size();x++){
+            grouplist.add(web2.get(x));
+            //webtotal.add(web2.get(x));
+        }
+
+        //webtotal.addAll(web);
+        //webtotal.addAll(web2);
+        //Log.e("NUMBER", webtotal.size()+"");
+
+        for(int x=0;x<webtotal.size();x++){
+            Log.e("GROUPS ??", webtotal.get(x));
+        }
+
+        int[] imageIdtotal = new int[webtotal.size()];
+        for(int j=0;j<imageIdtotal.length;j++) {
+            imageIdtotal[j] = R.drawable.groups;
+        }
+
+        adaptertotal = new GroupAdapter(getApplicationContext(), webtotal, imageIdtotal);
+        totalGrid.setAdapter(adaptertotal);
+
+        //TEXT CHANGE LISTENER
+        searchBar.addTextChangeListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                previousLength = charSequence.length();
+            }
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                //SEARCH FILTER
+                //adapter.getFilter().filter(charSequence);
+                backSpace = previousLength > charSequence.length();
+
+                if(charSequence.length()==0){
+                    yourgroup.setVisibility(View.VISIBLE);
+                    allgroup.setVisibility(View.VISIBLE);
+                    yourGrid.setVisibility(View.VISIBLE);
+                    mainGrid.setVisibility(View.VISIBLE);
+                    totalGrid.setVisibility(View.GONE);
+                    totalgroup.setVisibility(View.GONE);
+                    int[] imageId = new int[web.size()];
+                    for(int j=0;j<imageId.length;j++) {
+                        imageId[j] = R.drawable.groups;
+                    }
+
+                    adapter = new GroupAdapter(getApplicationContext(), web, imageId);
+                    int[] imageId2 = new int[web2.size()];
+                    for(int j=0;j<imageId2.length;j++) {
+                        imageId2[j] = R.drawable.groups;
+                    }
+
+                    adapter2 = new GroupAdapter(getApplicationContext(), web2, imageId2);
+                    //adapter.setWeb(web);
+                    //adapter2.setWeb(web2);
+                }else{
+                    yourgroup.setVisibility(View.GONE);
+                    allgroup.setVisibility(View.GONE);
+                    yourGrid.setVisibility(View.GONE);
+                    mainGrid.setVisibility(View.GONE);
+                    totalGrid.setVisibility(View.VISIBLE);
+                    totalgroup.setVisibility(View.VISIBLE);
+
+                    //Toast.makeText(getApplicationContext(), "ontextchanged "+charSequence, Toast.LENGTH_LONG).show();
+                    Log.e("GROUPS ??", "ontextchanged "+charSequence);
+                    //Log.e("GROUPS? ?", "1 "+adapter.getCount()+" 2 "+adapter2.getCount());
+
+                    if (backSpace) {
+                        adaptertotal.setWeb(webtotal);
+                        //adapter2.setWeb(new ArrayList<String>());
+                        //Log.e("GROUPS? ? ?", "1 "+adapter.getCount()+" 2 "+adapter2.getCount());
+                        adaptertotal.getFilter().filter(charSequence);
+                        //Log.e("GROUPS after ?", "1 "+adapter.getCount()+" 2 "+adapter2.getCount());
+                    }else{
+                        adaptertotal.setWeb(webtotal);
+                        //adapter2.setWeb(new ArrayList<String>());
+                        //Log.e("GROUPS? ? ?", "1 "+adapter.getCount()+" 2 "+adapter2.getCount());
+                        adaptertotal.getFilter().filter(charSequence);
+                        //Log.e("GROUPS after", "1 "+adapter.getCount()+" 2 "+adapter2.getCount());
+                    }
+                }
+
+            }
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)){
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //Inflate the menu; this adds items to the action bar if it is present.
+        //getMenuInflater().inflate(R.menu.profile, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        /*if (id == R.id.action_settings) {
+            return true;
+        }*/
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+        if (id == R.id.nav_home_events) {
+            /*if(dorr.equals("donor")){
+                Toast.makeText(getApplicationContext(),"Welcome Donor",Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(getApplicationContext(), WelcomeDonor.class));
+            }else
+                startActivity(new Intent(getApplicationContext(), WelcomeReceiver.class));*/
+            startActivity(new Intent(getApplicationContext(), Events.class));
+        } else if (id == R.id.nav_requests_events) {
+            /*Toast.makeText(getApplicationContext(),"Profile Receiver",Toast.LENGTH_SHORT).show();
+            if(dorr.equals("donor")){*/
+            Toast.makeText(getApplicationContext(),"Requests",Toast.LENGTH_SHORT).show();
+            Intent i = new Intent(this, Requests.class);
+                /*Bundle bundle = new Bundle();
+                bundle.putString("username", username1);
+                i.putExtras(bundle);*/
+
+            startActivity(i);
+            //startActivity(new Intent(getApplicationContext(), ProfileDonor.class));
+            /*}else
+                startActivity(new Intent(getApplicationContext(), ProfileReceiver.class));*/
+        } else if (id == R.id.nav_groups_events){
+            startActivity(new Intent(getApplicationContext(), Groups.class));
+        }
+        else if (id == R.id.nav_logout_events) {
+            session.logoutUser();
+        }
+
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
